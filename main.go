ba488943e01103d32e1d3b1a60a13b3e4268e140
Andres Alvarez <andres15alvarez@gmail.com>
2023-01-23 15:32:54 -0400

    Fix main

--- main.go
+++ main.go
@@ -1,27 +1,12 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"os"
-
+	"github.com/andres15alvarez/ticket_manager/app"
 	"github.com/joho/godotenv"
-	"github.com/uptrace/bun"
-	"github.com/uptrace/bun/dialect/pgdialect"
-	"github.com/uptrace/bun/driver/pgdriver"
 )
 
 func main() {
 	godotenv.Load()
-	dsn := fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		os.Getenv("DATABASE_USER"),
-		os.Getenv("DATABASE_PASSWORD"),
-		os.Getenv("DATABASE_HOST"),
-		os.Getenv("DATABASE_PORT"),
-		os.Getenv("DATABASE_NAME"),
-	)
-	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
-	db := bun.NewDB(sqldb, pgdialect.New())
-	db.Ping()
+	app := app.CreateApp()
+	app.Server.Run()
 }
