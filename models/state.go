e11db1689405c7175308c0d0c66ec3a3c8af847a
Andres Alvarez <andres15alvarez@gmail.com>
2023-01-24 10:35:03 -0400

    Create function to get all states

--- models/state.go
+++ models/state.go
@@ -1,9 +1,23 @@
 package models
 
-import "github.com/uptrace/bun"
+import (
+	"context"
+
+	"github.com/uptrace/bun"
+)
 
 type State struct {
 	bun.BaseModel `bun:"table:state"`
-	ID            int64
-	Name          string
+	ID            int64  `json:"id"`
+	Name          string `json:"name"`
+}
+
+func GetAllStates(db *bun.DB) ([]*State, error) {
+	var states []*State
+	state := State{}
+	err := db.NewSelect().Model(&state).Scan(context.Background(), &states)
+	if err != nil {
+		return nil, err
+	}
+	return states, nil
 }
