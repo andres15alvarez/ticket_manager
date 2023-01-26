3cd50589038662c7d3ccc7a707fe0ab9dec916f3
Andres Alvarez <andres15alvarez@gmail.com>
2023-01-26 11:55:25 -0400

    Create functions to create user and get user by email}

--- models/user.go
+++ models/user.go
@@ -2,19 +2,21 @@ package models
 
 import (
 	"context"
+	"log"
 	"time"
 
+	"github.com/andres15alvarez/ticket_manager/utils"
 	"github.com/uptrace/bun"
 )
 
 type User struct {
 	bun.BaseModel `bun:"table:user,alias:u"`
 	ID            int64      `bun:",pk,autoincrement" json:"id"`
-	Username      string     `json:"username"`
-	Password      string     `json:"password"`
+	Username      string     `bun:",unique" json:"username"`
+	Password      string     `json:"-"`
 	FirstName     string     `json:"first_name"`
 	LastName      string     `json:"last_name"`
-	Email         string     `json:"email"`
+	Email         string     `bun:",unique" json:"email"`
 	UserTypeID    int64      `json:"user_type_id"`
 	UserType      UserType   `bun:"rel:belongs-to,join:user_type_id=id" json:"user_type"`
 	CreatedAt     time.Time  `bun:",nullzero,notnull,default:current_timestamp" json:"created_at"`
@@ -41,4 +43,35 @@ func GetUserByID(db *bun.DB, id int64) (*User, error) {
 		return nil, err
 	}
 	return &user, nil
+}
+
+func GetUserByEmail(db *bun.DB, email string) (*User, error) {
+	user := User{}
+	err := db.NewSelect().Model(&user).Where("email = ?", email).
+		Relation("UserType").Scan(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func CreateUser(db *bun.DB, user *User) (*User, error) {
+	hashedPassword, err := utils.HashPassword(user.Password)
+	if err != nil {
+		return nil, err
+	}
+	user.UserTypeID = utils.UserTypeID
+	user.Password = hashedPassword
+	user.CreatedAt = time.Now()
+	user.UpdatedAt = time.Now()
+	res, err := db.NewInsert().Model(user).Exec(context.Background())
+	log.Printf("User Created: %v", res)
+	if err != nil {
+		return nil, err
+	}
+	userCreated, err := GetUserByEmail(db, user.Email)
+	if userCreated == nil {
+		return user, nil
+	}
+	return userCreated, nil
 }
