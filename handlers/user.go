5e396c0d6e6cf71e66d077efe6ee469d7a637548
Andres Alvarez <andres15alvarez@gmail.com>
2023-01-26 11:55:47 -0400

    Register create user handler

--- handlers/user.go
+++ handlers/user.go
@@ -44,3 +44,18 @@ func GetUserHandler(c *gin.Context) {
 	}
 
 }
+
+func CreateUserHandler(c *gin.Context) {
+	db := config.GetDB()
+	var user models.User
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	userCreated, err := models.CreateUser(db, &user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, userCreated)
+}
